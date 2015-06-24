c991d8395dbdc31f15220ede9296ef9748a6894d
Fabian Wohlgemuth <mail@wohfab.de>
2015-06-24 16:46:08 +0200

    added remove_links() function
    splitted episode data
    
    prepared season_dict {}

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 import wikipydia
 import re
 
+import xml.etree.ElementTree as ElTree
 
 # tvShow = 'Vikings'
 # tvShow = 'Sherlock'
@@ -66,6 +67,14 @@ def fix_links(file_name):
     write_html('tmp/fixed_links.html', fixed_links)
 
 
+def remove_links(file_name):
+    content = read_html(file_name)
+    removed_links = re.sub(r'</?a.*?>', '', content)
+    # remove NEW LINE HTML tags
+    removed_n = re.sub(r'<br />\n', ' ', removed_links)
+    write_html('tmp/removed_links.html', removed_n)
+
+
 def write_html(file_name, content):
     f = open(file_name, 'w')
     f.write(content)
@@ -96,40 +105,30 @@ def get_table_rows(table):
     return table_rows
 
 
-def get_episodes(table):
+def get_number_of_episodes(table):
     episodes = re.findall('vevent', table)
-    return episodes
-
-
-def get_table_row_data(table):
-    episodes = get_episodes(table)
-    table_row_data = re.compile(r'<td.*>')
-    write_html('tmp/strip.html', table_row_data.sub('', ''.join(episodes)))
+    return len(episodes)
 
 
 def test():
-    tv_show = input('TV Show: ')
+    tv_show = "The Blacklist"  # input('TV Show: ')
+    season = 1  # input('Season: ')
     crawl(tv_show)
     get_tables('tmp/output.html')
-    fix_links('tmp/tables.html')
+    remove_links('tmp/tables.html')
 
-# test()
+    # fix_links('tmp/tables.html')
+    # print(get_table_rows(get_tables('tmp/removed_links.html')[season - 1]))
 
-vikings_s1 = ['<tr style="color:white;"><th style="background:#33373b;width:6%">No. in<br />series</th><th style="background:#33373b;width:6%">No. in<br />season</th><th style="background:#33373b;width:18%">Title</th><th style="background:#33373b;width:20%">Directed by</th><th style="background:#33373b;width:20%">Written by</th><th style="background:#33373b;width:17%">Original air date</th><th style="background:#33373b;width:13%">U.S. viewers<br />(million)</th></tr>',
-              '<tr class="vevent" style="text-align:center;background:inherit"><th scope="row" id="ep1" style="text-align:center">1</th><td>1</td><td class="summary" style="text-align:left">"Rites of Passage"</td><td><a href="http://en.wikipedia.org/wiki/Johan_Renck" title="Johan Renck" class="mw-redirect">Johan Renck</a></td><td><a href="http://en.wikipedia.org/wiki/Michael_Hirst_(writer)" title="Michael Hirst (writer)">Michael Hirst</a></td><td>March&#160;3,&#160;2013<span style="display:none">&#160;(<span class="bday dtstart published updated">2013-03-03</span>)</span></td><td>6.21<sup id="cite_ref-4" class="reference"><a href="#cite_note-4"><span>[</span>4<span>]</span></a></sup></td></tr>',
-              '<tr class="vevent" style="text-align:center;background:inherit"><th scope="row" id="ep2" style="text-align:center">2</th><td>2</td><td class="summary" style="text-align:left">"Wrath of the Northmen"</td><td>Johan Renck</td><td>Michael Hirst</td><td>March&#160;10,&#160;2013<span style="display:none">&#160;(<span class="bday dtstart published updated">2013-03-10</span>)</span></td><td>4.62<sup id="cite_ref-5" class="reference"><a href="#cite_note-5"><span>[</span>5<span>]</span></a></sup></td></tr>',
-              '<tr class="vevent" style="text-align:center;background:inherit"><th scope="row" id="ep3" style="text-align:center">3</th><td>3</td><td class="summary" style="text-align:left">"Dispossessed"</td><td>Johan Renck</td><td>Michael Hirst</td><td>March&#160;17,&#160;2013<span style="display:none">&#160;(<span class="bday dtstart published updated">2013-03-17</span>)</span></td><td>4.83<sup id="cite_ref-6" class="reference"><a href="#cite_note-6"><span>[</span>6<span>]</span></a></sup></td></tr>',
-              '<tr class="vevent" style="text-align:center;background:inherit"><th scope="row" id="ep4" style="text-align:center">4</th><td>4</td><td class="summary" style="text-align:left">"Trial"</td><td><a href="http://en.wikipedia.org/wiki/Ciaran_Donnelly_(director)" title="Ciaran Donnelly (director)">Ciarn Donnelly</a></td><td>Michael Hirst</td><td>March&#160;24,&#160;2013<span style="display:none">&#160;(<span class="bday dtstart published updated">2013-03-24</span>)</span></td><td>4.54<sup id="cite_ref-7" class="reference"><a href="#cite_note-7"><span>[</span>7<span>]</span></a></sup></td></tr>',
-              '<tr class="vevent" style="text-align:center;background:inherit"><th scope="row" id="ep5" style="text-align:center">5</th><td>5</td><td class="summary" style="text-align:left">"Raid"</td><td>Ciarn Donnelly</td><td>Michael Hirst</td><td>March&#160;31,&#160;2013<span style="display:none">&#160;(<span class="bday dtstart published updated">2013-03-31</span>)</span></td><td>4.74<sup id="cite_ref-8" class="reference"><a href="#cite_note-8"><span>[</span>8<span>]</span></a></sup></td></tr>',
-              '<tr class="vevent" style="text-align:center;background:inherit"><th scope="row" id="ep6" style="text-align:center">6</th><td>6</td><td class="summary" style="text-align:left">"Burial of the Dead"</td><td>Ciarn Donnelly</td><td>Michael Hirst</td><td>April&#160;7,&#160;2013<span style="display:none">&#160;(<span class="bday dtstart published updated">2013-04-07</span>)</span></td><td>3.31<sup id="cite_ref-9" class="reference"><a href="#cite_note-9"><span>[</span>9<span>]</span></a></sup></td></tr>',
-              '<tr class="vevent" style="text-align:center;background:inherit"><th scope="row" id="ep7" style="text-align:center">7</th><td>7</td><td class="summary" style="text-align:left">"A King\'s Ransom"</td><td><a href="http://en.wikipedia.org/wiki/Ken_Girotti" title="Ken Girotti">Ken Girotti</a></td><td>Michael Hirst</td><td>April&#160;14,&#160;2013<span style="display:none">&#160;(<span class="bday dtstart published updated">2013-04-14</span>)</span></td><td>3.42<sup id="cite_ref-10" class="reference"><a href="#cite_note-10"><span>[</span>10<span>]</span></a></sup></td></tr>',
-              '<tr class="vevent" style="text-align:center;background:inherit"><th scope="row" id="ep8" style="text-align:center">8</th><td>8</td><td class="summary" style="text-align:left">"Sacrifice"</td><td>Ken Girotti</td><td>Michael Hirst</td><td>April&#160;21,&#160;2013<span style="display:none">&#160;(<span class="bday dtstart published updated">2013-04-21</span>)</span></td><td>3.85<sup id="cite_ref-11" class="reference"><a href="#cite_note-11"><span>[</span>11<span>]</span></a></sup></td></tr>',
-              '<tr class="vevent" style="text-align:center;background:inherit"><th scope="row" id="ep9" style="text-align:center">9</th><td>9</td><td class="summary" style="text-align:left">"All Change"</td><td>Ken Girotti</td><td>Michael Hirst</td><td>April&#160;28,&#160;2013<span style="display:none">&#160;(<span class="bday dtstart published updated">2013-04-28</span>)</span></td><td>3.58<sup id="cite_ref-12" class="reference"><a href="#cite_note-12"><span>[</span>12<span>]</span></a></sup></td></tr>']
+    season_dict = {}
 
-vikings_s1_e1 = vikings_s1[1]
+    print(get_number_of_episodes(get_tables('tmp/removed_links.html')[0]))
 
-print(vikings_s1[0])
+    get_table_rows(get_tables('tmp/removed_links.html')[season - 1])
+    tags = get_table_rows(get_tables('tmp/removed_links.html')[season - 1])[0]
+    tags = ''.join(ElTree.fromstring(tags).itertext())
 
-t = re.compile(r'</?t.*?>')
-print(t.sub('', vikings_s1_e1))
+    print(tags)
 
+
+test()
